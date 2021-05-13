5fd7fc02937d1e0888fb3d22653cea88f4d8f4a7
jingjiangtao <15210717655@163.com>
2021-05-13 09:05:04 +0800

    Add project files.

---
+++ CustomizeActionFilter/ActionFilters/VersionCheckAttribute.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomizeActionFilter.ActionFilters
+{
+    [Serializable, AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class VersionCheckAttribute : Attribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (HasIgnoreVersionCheck(context))
+            {
+                return;
+            }
+
+            if (!context.HttpContext.Request.Headers.ContainsKey("version"))
+            {
+                context.Result = new BadRequestObjectResult("Error: incorrect version");
+                return;
+            }
+
+            string headVersionStr = context.HttpContext.Request.Headers["version"].FirstOrDefault();
+            var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            string confVersionStr = configuration.GetValue<string>("VersionFilter");
+            if (headVersionStr != confVersionStr)
+            {
+                context.Result = new BadRequestObjectResult("Error: incorrect version");
+                return;
+            }
+        }
+
+        private bool HasIgnoreVersionCheck(ActionExecutingContext context)
+        {
+            IList<IFilterMetadata> filters = context.Filters;
+            foreach (IFilterMetadata filter in filters)
+            {
+                if (filter is IgnoreVersionCheckAttribute)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ CustomizeActionFilter/Controllers/SampleController.cs
@@ -0,0 +1,28 @@
+﻿using CustomizeActionFilter.ActionFilters;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomizeActionFilter.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [VersionCheck]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet("NeedVersionFilter")]
+        public IActionResult NeedVersionFilter()
+        {
+            return Ok("OK: Need Version");
+        }
+
+        [IgnoreVersionCheck]
+        [HttpGet("NoNeedVersionFilter")]
+        public IActionResult NoNeedVersionFilter()
+        {
+            return Ok("OK: No Need Version");
+        }
+    }
+}
